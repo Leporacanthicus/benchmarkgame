5be9423fd8d781cc4b20f99ad301218601dbea2c
Mats Petersson <mats@planetcatfish.com>
2016-05-02 22:07:10 +0100

    Make spectralnorm compile

--- spectralnorm.pas
+++ spectralnorm.pas
@@ -7,31 +7,38 @@
 
 program spectralNorm;
 
+const
+   MAXSIZE =  16384;
 
-var n,i : integer;
-    u,v,tmp : array of double;
-    vBv,vv : double;
+
+type
+   values = array [1..MAXSIZE] of double;
+
+var
+   n,i	   : integer;
+   u,v,tmp : 
+   vBv,vv  : double;
 
 function A(i,j : integer): double; inline;
 begin
   A := 1 / ((i+j)*(i+j+1) div 2 + i+1);
 end;
 
-procedure mulAv(var v, Av : array of double);
+procedure mulAv(var v, Av : values);
 var i,j : integer;
 begin
-  for i := low(Av) to high(Av) do
+  for i := 1 to n do
   begin
     Av[i] := 0.0;
-    for j := low(v) to high(v) do
+    for j := 1 to n do
       Av[i] := Av[i] + A(i,j) * v[j];
   end;
 end;
 
-procedure mulAtv(var v, Atv : array of double);
+procedure mulAtv(var v, Atv : values);
 var i,j : integer;
 begin
-  for i := low(Atv) to high(Atv) do
+  for i := 1 to n do
   begin
     Atv[i] := 0.0;
     for j := low(v) to high(v) do
@@ -39,23 +46,20 @@ begin
   end;
 end;
 
-procedure mulAtAv(var v, AtAv : array of double);
+procedure mulAtAv(var v, AtAv : values);
 begin
   mulAv(v, tmp);
   mulAtv(tmp, AtAv);
 end;
 
 begin
-  Val(paramstr(1), n, i);
-  SetLength(u, n);
-  SetLength(v, n);
-  SetLength(tmp, n);
+  Val(paramstr(1), n);
 
-  for i := low(u) to high(u) do u[i] := 1.0;
+  for i := 1 to n do u[i] := 1.0;
 
   for i := 1 to 10 do begin mulAtAv(u,v); mulAtAv(v,u) end;
 
-  for i := low(u) to high(u) do
+  for i := 1 to n do
   begin
     vBv := vBv + u[i]*v[i];
     vv  := vv  + v[i]*v[i];
